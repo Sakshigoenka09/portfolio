acd0c35e58d9e6f1df2cbba639c4057a260e4c95
Sakshi Goenka <sakshigoenka2752@gmail.com>
2026-04-06 16:03:38 +0530

    Revert Public Key to lowercase L

--- src/App.jsx
+++ src/App.jsx
@@ -114,7 +114,7 @@ function App() {
     setSubmitStatus(null);
 
     // Sending email via EmailJS
-    emailjs.sendForm('service_p9e325a', 'template_aolf7gi', formRef.current, 'vDFL8lhx-IRgy5AcV')
+    emailjs.sendForm('service_p9e325a', 'template_aolf7gi', formRef.current, 'vDFL8lhx-lRgy5AcV')
       .then((result) => {
           setSubmitStatus('success');
           setIsSubmitting(false);
